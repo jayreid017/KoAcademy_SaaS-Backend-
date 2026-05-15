9dff0e80b91ad7a678432a2e713942e13eaf4a0c
jay <bacurinjay07@gmail.com>
2026-05-15 11:21:34 +0800

    feat: install Laravel Sanctum and configure CORS for local development environments

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/test', function () {
+    return response()->json([
+        'message' => 'Laravel API working'
+    ]);
+}); 
